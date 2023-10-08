025fec8a9d564859a9a59126b0669fdf2fd694cf
Danielku15 <danielku15@coderline.net>
2023-10-08 16:53:14 +0200

    Manual resolve of latest c++ include dir and fallback

--- build/Build.Linux.cs
+++ build/Build.Linux.cs
@@ -1,4 +1,7 @@
 using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using Nuke.Common.IO;
 using Nuke.Common.Tooling;
 
 partial class Build
@@ -9,7 +12,7 @@ partial class Build
         {
             return;
         }
-        
+
         var bash = ToolResolver.GetPathTool("bash");
         var sudo = ToolResolver.GetPathTool("sudo");
         var dependenciesScript = SkiaPath / "tools" / "install_dependencies.sh";
@@ -18,23 +21,28 @@ partial class Build
         var arch = Architecture.LinuxArch;
         sudo($"apt-get update");
         sudo("apt-get upgrade -y");
-                    
+
         sudo($"dpkg -add-architecture {arch}");
 
         if (arch == Architecture.Arm || arch == Architecture.Arm64)
         {
             sudo("sed -i \"s/deb /deb [arch=amd64,i386] /\" /etc/apt/sources.list");
             sudo("sed -i \"s/deb-src /deb-src [arch=amd64,i386] /\" /etc/apt/sources.list");
-            sudo($"su -c \"echo 'deb [arch={arch}] http://ports.ubuntu.com/ubuntu-ports/ jammy main multiverse universe' >> /etc/apt/sources.list\"");
-            sudo($"su -c \"echo 'deb [arch={arch}] http://ports.ubuntu.com/ubuntu-ports/ jammy-security main multiverse universe' >> /etc/apt/sources.list\"");
-            sudo($"su -c \"echo 'deb [arch={arch}] http://ports.ubuntu.com/ubuntu-ports/ jammy-backports main multiverse universe' >> /etc/apt/sources.list\"");
-            sudo($"su -c \"echo 'deb [arch={arch}] http://ports.ubuntu.com/ubuntu-ports/ jammy-updates main multiverse universe' >> /etc/apt/sources.list\"");
+            sudo(
+                $"su -c \"echo 'deb [arch={arch}] http://ports.ubuntu.com/ubuntu-ports/ jammy main multiverse universe' >> /etc/apt/sources.list\"");
+            sudo(
+                $"su -c \"echo 'deb [arch={arch}] http://ports.ubuntu.com/ubuntu-ports/ jammy-security main multiverse universe' >> /etc/apt/sources.list\"");
+            sudo(
+                $"su -c \"echo 'deb [arch={arch}] http://ports.ubuntu.com/ubuntu-ports/ jammy-backports main multiverse universe' >> /etc/apt/sources.list\"");
+            sudo(
+                $"su -c \"echo 'deb [arch={arch}] http://ports.ubuntu.com/ubuntu-ports/ jammy-updates main multiverse universe' >> /etc/apt/sources.list\"");
         }
 
         sudo($"apt-get update");
         sudo($"aptitude install -y crossbuild-essential-{arch} libstdc++-11-dev-{arch}-cross");
         sudo($"aptitude install -y libfontconfig-dev:{arch} libgl1-mesa-dev:{arch} libglu1-mesa-dev:{arch}");
     }
+
     void BuildLibAlphaSkiaLinux()
     {
         var gnArgs = new Dictionary<string, string>();
@@ -122,10 +130,19 @@ partial class Build
             var init = $"'--sysroot={sysroot}', '--target={crossCompileTargetArch}'";
             var bin = $"'-B{sysroot}/bin/' ";
             var libs = $"'-L{sysroot}/lib/' ";
-            var includes = 
+
+            AbsolutePath sysRootPath = sysroot;
+            var newestCpp = Directory.EnumerateDirectories(sysRootPath / "include" / "c++")
+                .Select(Path.GetFileName)
+                .OrderByDescending(x => x)
+                .First();
+
+
+            var includes =
                 $"'-I{sysroot}/include', " +
-                $"'-I{sysroot}/include/c++/current', " +
-                $"'-I{sysroot}/include/c++/current/{crossCompileToolchainArch}' ";
+                $"'-I{sysroot}/include/c++/{newestCpp}', " +
+                $"'-I{sysroot}/include/c++/{newestCpp}/{crossCompileToolchainArch}', " +
+                $"'-I/usr/include/' ";
 
             AppendToFlagList(gnArgs, "extra_asmflags", $"{init}, '-no-integrated-as', {bin}, {includes}");
             AppendToFlagList(gnArgs, "extra_ldflags", $"{init}, {bin}, {libs}");
