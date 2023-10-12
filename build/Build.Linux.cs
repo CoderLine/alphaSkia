78639eb81db86703dc74a5ae0f71edf17f81b2db
Danielku15 <danielku15@coderline.net>
2023-10-12 20:45:00 +0200

    Disable libnode and check results

--- build/Build.Linux.cs
+++ build/Build.Linux.cs
@@ -63,7 +63,8 @@ partial class Build
         // dependent libraries
         var linuxArchSuffix = string.IsNullOrEmpty(linuxArch) ? "" : $":{linuxArch}";
         installDependencies.AppendLine("echo Installing libs");
-        installDependencies.AppendLine($"aptitude install -y libfontconfig-dev{linuxArchSuffix} libgl1-mesa-dev{linuxArchSuffix} libglu1-mesa-dev{linuxArchSuffix} freeglut3-dev{linuxArchSuffix} libnode-dev{linuxArchSuffix}");
+        installDependencies.AppendLine($"aptitude install -y libfontconfig-dev{linuxArchSuffix} libgl1-mesa-dev{linuxArchSuffix} libglu1-mesa-dev{linuxArchSuffix} freeglut3-dev{linuxArchSuffix}");
+        // TODO libnode-dev{linuxArchSuffix}
 
         var scriptFile = TemporaryDirectory / "install_dependencies.sh";
         File.WriteAllText(scriptFile, installDependencies.ToString());
