01708070cea40a8aabd36a0497fc242b9d63f9d8
Danielku15 <danielku15@coderline.net>
2023-10-15 20:22:41 +0200

    Set min macOS version

--- build/Build.MacOs.cs
+++ build/Build.MacOs.cs
@@ -4,7 +4,7 @@ partial class Build
 {
     void SetClangMacOs(Dictionary<string, string> gnArgs)
     {
-        AppendToFlagList(gnArgs, "extra_cflags", "'-DHAVE_ARC4RANDOM_BUF', '-stdlib=libc++'");
+        AppendToFlagList(gnArgs, "extra_cflags", "'-DHAVE_ARC4RANDOM_BUF', '-stdlib=libc++', '-mmacosx-version-min=10.15'");
         AppendToFlagList(gnArgs, "extra_ldflags", "'-stdlib=libc++'");
     }
 }
